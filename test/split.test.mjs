4f16d4097a4c4008aff146f7eec98293763470ac
Riceball LEE <snowyu.lee@gmail.com>
2025-01-14 20:49:44 +0800

    chore: minor changes

--- test/split.test.mjs
+++ test/split.test.mjs
@@ -22,4 +22,20 @@ describe("split", async () => {
     options.hmm = false
     expect(split("中国南宁美比尔科技有限公司", options)).toEqual([ '中国', '南宁', '美', '比尔', '科技', '有限公司' ]);
   });
+
+  it("should split with all options", () => {
+    const options = {}
+    options.mode = 'All'
+    options.hmm = true
+    const result = split("中国南宁美比尔科技有限公司", options);
+    expect(result).toEqual([
+      '中',       '中国', '国',
+      '南',       '南宁', '宁',
+      '美',       '比',   '比尔',
+      '尔',       '科',   '科技',
+      '技',       '有',   '有限',
+      '有限公司', '限',   '公',
+      '公司',     '司'
+    ]);
+  });
 });
