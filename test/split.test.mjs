0737f54dfc2fac6272b31fb971e1a46db81e16c2
Riceball LEE <snowyu.lee@gmail.com>
2025-01-15 07:38:46 +0800

    test: more test

--- test/split.test.mjs
+++ test/split.test.mjs
@@ -23,6 +23,21 @@ describe("split", async () => {
     expect(split("中国南宁美比尔科技有限公司", options)).toEqual([ '中国', '南宁', '美', '比尔', '科技', '有限公司' ]);
   });
 
+  it.skip("should split with hmm option to true", () => {
+    const options = {}
+    options.mode = 'Default'
+    options.hmm = true
+    expect(split("今天天气真好", options)).toEqual([ '今天天气', '真', '好' ]);
+  });
+
+  it("should split with hmm true on complex sentence", () => {
+    const options = { mode: 'Default', hmm: true };
+    expect(split("他来到了网易杭研大厦", options)).toEqual([ '他', '来到', '了', '网易', '杭研', '大厦' ]);
+  });
+  it("should split with hmm false on complex sentence", () => {
+    const options = { mode: 'Default', hmm: false };
+    expect(split("他来到了网易杭研大厦", options)).toEqual([ '他', '来到', '了', '网易', '杭', '研', '大厦' ]);
+  });
   it("should split with all options", () => {
     const options = {}
     options.mode = 'All'
