590148ccf9f3c4c4349e859c961b0b6451a219de
Riceball LEE <snowyu.lee@gmail.com>
2025-12-01 08:56:27 +0800

    test(dict): test add word with freq and tag

--- test/dict.test.mjs
+++ test/dict.test.mjs
@@ -1,39 +1,67 @@
 import { describe, it, expect } from "vitest";
-import { addDefaultDict, addDict, clear, hasWord, addWord, removeWord, suggestFreq } from "..";
+import { addDefaultDict, addDict, clear, hasWord, addWord, removeWord, suggestFreq, tag } from "..";
 
 addDefaultDict()
 
 describe("dict", () => {
   it("should clear dict", () => {
+    try {
     expect(hasWord("我们")).toBeTruthy();
     clear();
     expect(hasWord("我们")).toBeFalsy();
-    addDefaultDict();
-    expect(hasWord("我们")).toBeTruthy();
+    } finally {
+      addDefaultDict();
+      expect(hasWord("我们")).toBeTruthy();
+    }
   });
+
   it("should add/remove word", () => {
     expect(hasWord("我们开始")).toBeFalsy();
     addWord("我们开始");
-    expect(hasWord("我们开始")).toBeTruthy();
-    removeWord("我们开始");
-    expect(hasWord("我们开始")).toBeFalsy();
+    try {
+      expect(hasWord("我们开始")).toBeTruthy();
+    } finally {
+      removeWord("我们开始");
+      expect(hasWord("我们开始")).toBeFalsy();
+    }
   });
 
   it("should add/remove word with freq", ()=> {
     addWord("我们开始", 300);
-    expect(suggestFreq("我们开始")).toBe(300)
-    removeWord("我们开始");
-    expect(hasWord("我们开始")).toBeFalsy()
-    addWord("我们开始", 301);
-    expect(suggestFreq("我们开始")).toBe(301)
-    expect(hasWord("中出")).toBeTruthy();
-    expect(suggestFreq("中出")).toBe(348)
-    addWord("中出", 300);
-    // But it's less than calculated freq 100
-    expect(suggestFreq("中出")).toBe(348)
-    addWord("中出", 500);
-    // Now it's significant enough
-    expect(suggestFreq("中出")).toBe(500)
+    try {
+      expect(suggestFreq("我们开始")).toBe(300)
+      removeWord("我们开始");
+      expect(hasWord("我们开始")).toBeFalsy()
+      addWord("我们开始", 301);
+      expect(suggestFreq("我们开始")).toBe(301)
+      expect(hasWord("中出")).toBeTruthy();
+      expect(suggestFreq("中出")).toBe(348)
+      addWord("中出", 300);
+      // But it's less than calculated freq 100
+      expect(suggestFreq("中出")).toBe(348)
+      addWord("中出", 500);
+      // Now it's significant enough
+      expect(suggestFreq("中出")).toBe(500)
+    } finally {
+      removeWord("我们开始");
+      removeWord("中出");
+      expect(hasWord("我们开始")).toBeFalsy()
+      expect(hasWord("中出")).toBeFalsy()
+    }
+  })
+
+  it("should add/remove word with freq and tag", ()=> {
+    addWord("哈基米", 302, "n");
+    try {
+      expect(suggestFreq("哈基米")).toBe(302)
+      let t = tag("哈基米")
+      expect(t).toHaveLength(1)
+      expect(t[0].word).toBe("哈基米")
+      expect(t[0].tag).toBe("n")
+    } finally {
+      removeWord("哈基米");
+      expect(hasWord("哈基米")).toBeFalsy()
+    }
   })
 
   it("should load dict", () => {
